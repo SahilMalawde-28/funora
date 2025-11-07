aef0570fd4961508c1e817f287a4c6c74b8cda12
Sahil Malawde <sahilmalawde49@gmail.com>
2025-11-07 19:01:11 +0530

    Updated MemoryGame.tsx

--- src/components/games/MemoryGame.tsx
+++ src/components/games/MemoryGame.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { supabase } from "../../lib/supabaseClient";
+import { supabase } from "../../lib/supabase";
 import { MemoryGameState, Tile } from "../../lib/gameLogic";
 import { Room, Player } from "../../lib/supabase";
 
