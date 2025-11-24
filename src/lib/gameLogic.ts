b5c82d34551c282764eea3e92be9be356435f30e
Sahil Malawde <sahilmalawde49@gmail.com>
2025-11-24 22:53:53 +0530

    Updated gameLogic.ts

--- src/lib/gameLogic.ts
+++ src/lib/gameLogic.ts
@@ -2050,7 +2050,7 @@ const COLORS = [
   
 ];
 
-var randInt = (min: number, max: number) =>
+var randIntG = (min: number, max: number) =>
   Math.floor(Math.random() * (max - min + 1)) + min;
 
 export const shuffleArray = <T,>(arr: T[]) => {
