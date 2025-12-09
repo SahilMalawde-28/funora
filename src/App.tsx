84831632d0ff7f190c9d2294581843bc26a36139
Sahil Malawde <sahilmalawde49@gmail.com>
2025-12-10 00:14:09 +0530

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -42,7 +42,7 @@ function App() {
   // ==================================================
   // UPDATE PROFILE STATS
   // ==================================================
-  async const updateProfileStats = (gameId: string, didWin: boolean = false) => {
+  const updateProfileStats = (gameId: string, didWin: boolean = false) => {
     if (!profile) return;
 
     const updated = {
